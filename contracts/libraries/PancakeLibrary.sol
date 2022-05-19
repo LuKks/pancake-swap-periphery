ca5117f266da139612227a1619e4b801e784fb78
Lucas <lucasbarrena98@gmail.com>
2022-05-19 04:49:08 -0300

    Update PancakeLibrary.sol

--- contracts/libraries/PancakeLibrary.sol
+++ contracts/libraries/PancakeLibrary.sol
@@ -18,12 +18,12 @@ library PancakeLibrary {
     // calculates the CREATE2 address for a pair without making any external calls
     function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
         (address token0, address token1) = sortTokens(tokenA, tokenB);
-        pair = address(uint(keccak256(abi.encodePacked(
+        pair = address(uint160(uint(keccak256(abi.encodePacked(
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
                 hex'd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66' // init code hash
-            ))));
+            )))));
     }
 
     // fetches and sorts the reserves for a pair
